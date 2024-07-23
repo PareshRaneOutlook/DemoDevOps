e5191a748f92df452c568e84ce680cbb4a929c57
paresh.rane@outlook.com <paresh.rane@outlook.com>
2024-07-23 21:58:44 +0530

    updated requiement.txt

--- backend/djando_app/mainapp/local_settings.py
+++ backend/djando_app/mainapp/local_settings.py
@@ -1,11 +1,22 @@
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'predictiondb',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'predictiondb',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'USER': 'mysql',
+        'PASSWORD': 'mysql',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
